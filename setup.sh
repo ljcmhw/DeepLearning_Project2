dbd250e31673e03a738c76cf9568d02291dc812a
Qiyi0701 <160573260+Qiyi0701@users.noreply.github.com>
2025-04-17 09:53:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
+# Create and activate virtual environment, then install dependencies
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
 
